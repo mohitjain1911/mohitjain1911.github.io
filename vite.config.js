4109bccfb75ac13127f4c9fd82799a00728b869a
mohit.jain <mohit.jain@medimaze.in>
2025-10-01 10:33:42 +0530

    Made the path as /

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  // const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+    base: '/',
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
